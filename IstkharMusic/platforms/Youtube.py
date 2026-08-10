1916b4551444748f3f2e7bd7df610e7e25bbe779
Stark773 <akgameing452@gmail.com>
2026-08-10 12:20:40 +0545

    Update print statement from 'Hello' to 'Goodbye'

--- IstkharMusic/platforms/Youtube.py
+++ IstkharMusic/platforms/Youtube.py
@@ -9,7 +9,7 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from IstkharMusic import VideosSearch
+from py_yt import VideosSearch
 try:
     from py_yt import Recommendations as _PyYtRec
 except ImportError:
