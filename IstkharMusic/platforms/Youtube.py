7139f1eaf02e76d6736d8159fdad4739f9f6269d
Stark773 <akgameing452@gmail.com>
2026-08-10 14:26:37 +0545

    Update Youtube.py

--- IstkharMusic/platforms/Youtube.py
+++ IstkharMusic/platforms/Youtube.py
@@ -14,8 +14,8 @@ try:
     from py_yt import Recommendations as _PyYtRec
 except ImportError:
     _PyYtRec = None
-from Muskan_Music.helpers._store import is_on_off
-from Muskan_Music.helpers._fmt import time_to_seconds
+from IstkharMusic.helpers._store import is_on_off
+from IstkharMusic.helpers._fmt import time_to_seconds
 import os
 import glob
 import random
@@ -23,7 +23,7 @@ import logging
 import aiohttp
 import config
 from config import LOGGER_ID
-from Muskan_Music import app
+from IstkharMusic import app
 from config import BASE_URL, API_KEY
 from urllib.parse import urlparse, unquote
 
